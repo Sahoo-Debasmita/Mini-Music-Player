4cb384a01cd118674a5905be05419e695d08a148
Debasmita Sahoo <sdebasmita974@gmail.com>
2023-07-22 12:18:41 +0530

    Prevous button working

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 console.log("Welcome to the world of music");
+// All songs information
+let song = [
+{ SongName: "JayJagganath", FilePath: "songs/Jay Jagganath.mp3", Coverimg: "Images_of_music_app/cover5.jpg" },
+{ SongName: "Khokabhai",FilePath: "songs/Khokabhai.mp3", Coverimg: "Images_of_music_app/cover6.jpg" },
+{ SongName: "Dilwale", FilePath: "songs/Dilwale.mp3", Coverimg: "Images_of_music_app/cover7.jpg" },
+{ SongName: "Master", FilePath: "songs/Master.mp3", Coverimg: "Images_of_music_app/cover2.jpg" },
+]
 // Intitialization of all variables to use
-let index=document.getElementById("songs/Jay Jagganath.mp3");
-let audio = new Audio("songs/pasoori.mp3");
+let index=song[0].FilePath;
+let audio = new Audio(index);
 let Playbutton=document.getElementById("mainplay")
 let progressbar=document.getElementById("Progressbar")
 let gif=document.getElementById("gif")
 let SongItem=Array.from(document.getElementsByClassName("songItem"));
-// All songs information
-let song = [
-{ SongName: "Jay Jagganath", FilePath: "songs/1.mp3", Coverimg: "Images_of_music_app/cover5.jpg" },
-{ SongName: "Khoka Bhai special",FilePath: "song/2.mp3", Coverimg: "Images_of_music_app/cover6.jpg" },
-{ SongName: "Tukur tukur", FilePath: "songs/3.mp3", Coverimg: "Images_of_music_app/cover7.jpg" },
-{ SongName: "Master song", FilePath: "songs/4.mp3", Coverimg: "Images_of_music_app/cover2.jpg" },
-]
 // song Name & cover image change Handling
  SongItem.forEach((element,i)=>{
  element.getElementsByTagName("img")[0].src=song[i].Coverimg;
  element.getElementsByTagName("span")[0].innerHTML=song[i].SongName;
  })
-// play/pause event handling
+// Master play/pause event handling
 Playbutton.addEventListener('click',()=>{
     if (audio.paused || audio.currentTime<1) {
         audio.play();  
@@ -47,8 +47,9 @@ const makeallplay=()=>{
 }
 Array.from(document.getElementsByClassName("playsong")).forEach((element)=>{
     element.addEventListener('click',(e)=>{
-        makeallplay();
-         index=e.target.id;
+         makeallplay();
+         let select=e.target.id;//image id i.e the image icon
+         let index = song.find(songItem => songItem.SongName === select).FilePath;
          e.target.src="Images_of_music_app/pause.png"
          audio.src=`${index}`;
          audio.currentTime=0;
@@ -57,4 +58,23 @@ Array.from(document.getElementsByClassName("playsong")).forEach((element)=>{
          audio.play(); gif.style.opacity=1;
          mainplayicon.src="Images_of_music_app/pause.png";
     })
+})
+// Prevous Button handling
+document.getElementById("pre").addEventListener('click',()=>{
+    let maxindex = song.length;
+    let currentIndex = song.findIndex(songItem => songItem.FilePath === index);
+
+    if (currentIndex === 0) {
+        // If the current song is the first song, loop back to the last song
+        index = song[maxindex - 1].FilePath;
+    } else {
+        // Go to the previous song
+        index = song[currentIndex - 1].FilePath;
+    }
+     audio.src=`${index}`;
+     audio.currentTime=0;
+     const progressbartime=parseInt((audio.currentTime/audio.duration)*100);
+     progressbar.value=progressbartime;
+     audio.play(); gif.style.opacity=1;
+     mainplayicon.src="Images_of_music_app/pause.png";
 })
